8f8f7c0c8d2f73e86dad2e479975c06874e0dc72
Godsgift Akari <giftakari@yahoo.com>
2021-02-13 13:50:41 +1000

    components structure

---
+++ Components/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Footer,
+  FooterTab,
+  Left,
+  Right,
+  Body,
+  Title,
+} from 'native-base';
+
+const Layout = (props) => (
+  <Container>
+    <Header>
+      <Left>{props.left}</Left>
+      <Body>
+        <Title>{props.title}</Title>
+      </Body>
+      <Right>{props.right}</Right>
+    </Header>
+    {props.children}
+    <Footer>
+      <FooterTab>{props.footer}</FooterTab>
+    </Footer>
+  </Container>
+);
+
+export default Layout;
---
+++ Components/NoteContent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Card, CardItem, Body, H3, Text} from 'native-base';
+//import { not } from 'react-native-reanimated'
+
+export const NoteContent = ({note}) => {
+  return (
+    <Card>
+      <CardItem>
+        <Body>
+          <H3>{note.item.title}</H3>
+          <Text>
+            {note.item.content > 120
+              ? `${note.item.content.substr(0, 120)}`
+              : note.item.content}
+          </Text>
+        </Body>
+      </CardItem>
+    </Card>
+  );
+};
